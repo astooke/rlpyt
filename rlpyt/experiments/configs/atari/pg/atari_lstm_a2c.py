435bc8ec96b909d626c23d4fd8f7a14bb3ec12f7
AStooke <adam.stooke@gmail.com>
2019-05-17 10:05:09 -0700

    first lstm experiment

---
+++ rlpyt/experiments/configs/atari/pg/atari_lstm_a2c.py
@@ -0,0 +1,29 @@
+
+configs = dict()
+
+
+config = dict(
+    agent=dict(),
+    algo=dict(
+        discount=0.99,
+        learning_rate=7e-4,
+        value_loss_coeff=0.5,
+        entropy_loss_coeff=0.01,
+        clip_grad_norm=1.,
+    ),
+    env=dict(game="pong"),
+    model=dict(),
+    optim=dict(),
+    runner=dict(
+        n_steps=10e6,
+        # log_interval_steps=1e3,
+    ),
+    sampler=dict(
+        batch_T=20,
+        batch_B=32,
+        max_path_length=27000,
+        max_decorrelation_steps=1000,
+    ),
+)
+
+configs["0"] = config
---
+++ rlpyt/experiments/scripts/atari/pg/launch/launch_atari_lstm_a2c_cpu.py
@@ -0,0 +1,43 @@
+
+from rlpyt.utils.launching.affinity import encode_affinity
+from rlpyt.utils.launching.exp_launcher import run_experiments
+from rlpyt.utils.launching.variant import make_variants, VariantLevel
+
+script = "rlpyt/experiments/scripts/atari/pg/train/atari_lstm_a2c_cpu.py"
+default_config_key = "0"
+affinity_code = encode_affinity(
+    n_cpu_cores=6,
+    n_gpu=2,
+    hyperthread_offset=8,
+    n_socket=1,
+    # cpu_per_run=2,
+)
+runs_per_setting = 1
+experiment_title = "first_lstm_test"
+variant_levels = list()
+
+learning_rate = [7e-4, 1e-3]
+batch_T = [20, 50]
+values = list(zip(learning_rate, batch_T))
+names = ["test_{}lr_{}T".format(*v) for v in values]
+keys = [("algo", "learning_rate"), ("sampler", "batch_T")]
+variant_levels.append(VariantLevel(keys, values, names))
+
+
+games = ["pong"]
+values = list(zip(games))
+names = ["{}".format(*v) for v in values]
+keys = [("env", "game")]
+variant_levels.append(VariantLevel(keys, values, names))
+
+variants, log_dirs = make_variants(*variant_levels)
+
+run_experiments(
+    script=script,
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+    common_args=(default_config_key,),
+)
