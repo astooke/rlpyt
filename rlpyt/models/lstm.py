6417ad12f5c47461e30a6b31a126873f51971851
Adam Stooke <adam.stooke@gmail.com>
2019-06-14 17:08:30 -0700

    trying to subclass lstm

--- rlpyt/models/lstm.py
+++ rlpyt/models/lstm.py
@@ -8,6 +8,6 @@ class LstmModel(torch.nn.LSTM):
         if init_rnn_state is not None:  # [B,N,H] --> [N,B,H]
             init_rnn_state = tuple(hc.transpose(0, 1).contiguous()
                 for hc in init_rnn_state)
-        lstm_out, (hn, cn) = super().__call__(input, init_rnn_state)
+        lstm_out, (hn, cn) = torch.nn.LSTM.__call__(self, input, init_rnn_state)
         hn, cn = hn.transpose(0, 1), cn.transpose(0, 1)  # --> [B,N,H]
         return lstm_out, (hn, cn)
