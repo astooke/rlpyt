df825dd265d6441407fcaf8dba94239f6ad788bb
Adam Stooke <adam.stooke@gmail.com>
2019-06-16 18:21:01 -0700

    compositional space

---
+++ rlpyt/spaces/composition.py
@@ -0,0 +1,31 @@
+
+
+from rlpyt.spaces.base import Space
+
+
+class Composition(Space):
+
+    def __init__(self, spaces, NamedArrayTupleCls):
+        self._spaces = spaces
+        # Should define NamedArrayTupleCls in the module creating this space.
+        self._NamedArrayTupleCls = NamedArrayTupleCls
+
+    def sample(self, size, null=False, torchify=False):
+        return self._NamedArrayTupleCls(*(space.sample(size, null, torchify)
+            for space in self._spaces))
+
+    @property
+    def shape(self):
+        # Could just be namedtuple.
+        return self._NamedArrayTupleCls(*(space.shape for space in self._spaces))
+
+    @property
+    def names(self):
+        return self._NamedArrayTupleCls._fields
+
+    @property
+    def spaces(self):
+        return self._spaces
+
+    def __repr__(self):
+        return ", ".join(space.__repr__() for space in self._spaces)
