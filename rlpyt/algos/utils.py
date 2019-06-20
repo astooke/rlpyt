3a846b85b6bf610ca77834a4147678dfe291d4e3
AStooke <adam.stooke@gmail.com>
2019-06-20 13:44:27 -0700

    typo fix

--- rlpyt/algos/utils.py
+++ rlpyt/algos/utils.py
@@ -49,6 +49,7 @@ def discount_return_n_step(reward, done, n_step, discount, return_dest=None,
 
 
 def valid_from_done(done):
-    valid = torch.ones_like(done, dtype=torch.float)
+    done = done.type(torch.float)
+    valid = torch.ones_like(done)
     valid[1:] = 1 - torch.clamp(torch.cumsum(done[:-1], dim=0), max=1)
     return valid
