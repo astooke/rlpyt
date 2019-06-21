61537b6c4533b075d7585a6d9cd18c53f61ddf57
AStooke <adam.stooke@gmail.com>
2019-06-21 16:51:06 -0700

    added got exp

---
+++ rlpyt/experiments/scripts/atari/dqn/launch/got/launch_atari_r2d1_long_4tr_gravitar.py
@@ -0,0 +1,35 @@
+
+from rlpyt.utils.launching.affinity import encode_affinity
+from rlpyt.utils.launching.exp_launcher import run_experiments
+from rlpyt.utils.launching.variant import make_variants, VariantLevel
+
+script = "rlpyt/experiments/scripts/atari/dqn/train/atari_r2d1_gpu.py"
+affinity_code = encode_affinity(
+    n_cpu_cores=12,
+    n_gpu=1,
+    hyperthread_offset=20,
+    n_socket=1,
+)
+runs_per_setting = 2
+experiment_title = "atari_r2d1_long_4tr"
+variant_levels = list()
+
+games = ["gravitar"]
+values = list(zip(games))
+dir_names = ["{}".format(*v) for v in values]
+keys = [("env", "game")]
+variant_levels.append(VariantLevel(keys, values, dir_names))
+
+variants, log_dirs = make_variants(*variant_levels)
+
+default_config_key = "r2d1_long_4tr"
+
+run_experiments(
+    script=script,
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+    common_args=(default_config_key,),
+)
