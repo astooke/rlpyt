1e18f3c30ddf31d7f48488bc54967a41adec2bb7
AStooke <adam.stooke@gmail.com>
2019-07-26 14:20:22 -0700

    fixed _buffer_full on async replay

--- rlpyt/replays/async_.py
+++ rlpyt/replays/async_.py
@@ -1,5 +1,6 @@
 
 import multiprocessing as mp
+import ctypes
 
 from rlpyt.utils.synchronize import RWLock
 
@@ -11,14 +12,17 @@ class AsyncReplayBufferMixin(object):
         super().__init__(*args, shared_memory=True, **kwargs)
         self.async_t = mp.RawValue("l")  # Type c_long.
         self.rw_lock = RWLock()
+        self._async_buffer_full = mp.RawValue(ctypes.c_bool, False)
 
     def append_samples(self, *args, **kwargs):
         with self.rw_lock.write_lock:
             ret = super().append_samples(*args, **kwargs)
             self.async_t.value = self.t
+            self._async_buffer_full.value = self._buffer_full
         return ret
 
     def sample_batch(self, *args, **kwargs):
         with self.rw_lock:
             self.t = self.async_t.value
+            self._buffer_full = self._async_buffer_full.value
             return super().sample_batch(*args, **kwargs)
