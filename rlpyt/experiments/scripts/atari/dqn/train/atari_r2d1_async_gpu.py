f95448847a756daf0d079191691c3c46d6c3dfd5
AStooke <adam.stooke@gmail.com>
2019-08-01 04:14:27 -0700

    typo fix

--- rlpyt/experiments/scripts/atari/dqn/train/atari_r2d1_async_gpu.py
+++ rlpyt/experiments/scripts/atari/dqn/train/atari_r2d1_async_gpu.py
@@ -8,7 +8,7 @@ from rlpyt.samplers.async_.async_gpu_sampler import AsyncGpuSampler
 from rlpyt.samplers.async_.collectors import DbGpuResetCollector
 from rlpyt.envs.atari.atari_env import AtariEnv, AtariTrajInfo
 from rlpyt.algos.dqn.r2d1 import R2D1
-from rlpyt.agents.dqn.atari.atari_dqn_agent import AtariDqnAgent
+from rlpyt.agents.dqn.atari.atari_r2d1_agent import AtariR2d1Agent
 # from rlpyt.runners.minibatch_rl_eval import MinibatchRlEval
 from rlpyt.runners.async_rl import AsyncRlEval
 from rlpyt.utils.logging.context import logger_context
@@ -33,7 +33,7 @@ def build_and_train(slot_affinity_code, log_dir, run_ID, config_key):
         **config["sampler"]
     )
     algo = R2D1(optim_kwargs=config["optim"], **config["algo"])
-    agent = AtariDqnAgent(model_kwargs=config["model"], **config["agent"])
+    agent = AtariR2d1Agent(model_kwargs=config["model"], **config["agent"])
     runner = AsyncRlEval(
         algo=algo,
         agent=agent,
