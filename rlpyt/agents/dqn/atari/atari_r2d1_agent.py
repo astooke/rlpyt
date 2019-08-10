66d01245f04b03ee0c2dd5af22441f4ab7cc2fa8
AStooke <adam.stooke@gmail.com>
2019-08-10 10:56:48 -0700

    first alt sampler r2d1 script

--- rlpyt/agents/dqn/atari/atari_r2d1_agent.py
+++ rlpyt/agents/dqn/atari/atari_r2d1_agent.py
@@ -1,4 +1,4 @@
-from rlpyt.agents.dqn.r2d1_agent import R2d1Agent
+from rlpyt.agents.dqn.r2d1_agent import R2d1Agent, R2d1AlternatingAgent
 from rlpyt.models.dqn.atari_r2d1_model import AtariR2d1Model
 from rlpyt.agents.dqn.atari.mixin import AtariMixin
 
@@ -7,3 +7,10 @@ class AtariR2d1Agent(AtariMixin, R2d1Agent):
 
     def __init__(self, ModelCls=AtariR2d1Model, **kwargs):
         super().__init__(ModelCls=ModelCls, **kwargs)
+
+
+class AtariR2d1AlternatingAgent(AtariMixin, R2d1AlternatingAgent):
+
+    def __init__(self, ModelCls=AtariR2d1Model, **kwargs):
+        super().__init__(ModelCls=ModelCls, **kwargs)
+    
---
+++ rlpyt/experiments/scripts/atari/dqn/train/atari_r2d1_async_alt.py
@@ -0,0 +1,51 @@
+
+import sys
+
+from rlpyt.utils.launching.affinity import affinity_from_code
+# from rlpyt.samplers.cpu.parallel_sampler import CpuParallelSampler
+from rlpyt.samplers.async_.alternating_sampler import AsyncAlternatingSampler
+# from rlpyt.samplers.cpu.collectors import WaitResetCollector
+from rlpyt.samplers.async_.collectors import DbGpuResetCollector
+from rlpyt.envs.atari.atari_env import AtariEnv, AtariTrajInfo
+from rlpyt.algos.dqn.r2d1 import R2D1
+from rlpyt.agents.dqn.atari.atari_r2d1_agent import AtariR2d1AlternatingAgent
+# from rlpyt.runners.minibatch_rl_eval import MinibatchRlEval
+from rlpyt.runners.async_rl import AsyncRlEval
+from rlpyt.utils.logging.context import logger_context
+from rlpyt.utils.launching.variant import load_variant, update_config
+
+from rlpyt.experiments.configs.atari.dqn.atari_r2d1 import configs
+
+
+def build_and_train(slot_affinity_code, log_dir, run_ID, config_key):
+    affinity = affinity_from_code(slot_affinity_code)
+    config = configs[config_key]
+    variant = load_variant(log_dir)
+    config = update_config(config, variant)
+    config["eval_env"]["game"] = config["env"]["game"]
+
+    sampler = AsyncAlternatingSampler(
+        EnvCls=AtariEnv,
+        env_kwargs=config["env"],
+        CollectorCls=DbGpuResetCollector,
+        TrajInfoCls=AtariTrajInfo,
+        eval_env_kwargs=config["eval_env"],
+        **config["sampler"]
+    )
+    algo = R2D1(optim_kwargs=config["optim"], **config["algo"])
+    agent = AtariR2d1AlternatingAgent(model_kwargs=config["model"],
+        **config["agent"])
+    runner = AsyncRlEval(
+        algo=algo,
+        agent=agent,
+        sampler=sampler,
+        affinity=affinity,
+        **config["runner"]
+    )
+    name = "async_alt_" + config["env"]["game"]
+    with logger_context(log_dir, run_ID, name, config):
+        runner.train()
+
+
+if __name__ == "__main__":
+    build_and_train(*sys.argv[1:])
