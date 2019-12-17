8e927941881a6c86c9497d46e8a33551417a23a8
AStooke <adam.stooke@gmail.com>
2019-12-17 15:42:55 -0800

    docs work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,4 +59,5 @@ master_doc = 'index'
 
 autodoc_member_order = 'bysource'
 
-autodoc_mock_imports = ['torch', 'numpy', 'psutil', 'opencv', 'atari-py', 'pyprind']
+autodoc_mock_imports = ['torch', 'numpy', 'psutil', 'opencv', 'atari-py', 'pyprind',
+    'torch.autograd.Function.apply']
